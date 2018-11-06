5098551e27882881c5105bd42007692d52b6d600
Gladson <gladsonsouza@live.com>
2018-11-06 07:54:05 -0400

    Feito

--- src/TelaAlterarDepartamento.java
+++ src/TelaAlterarDepartamento.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -16,19 +17,16 @@ public class TelaAlterarDepartamento extends JPanel {
 	private JTextField txtCodigo;
 	private JTextField tfNome;
 	private JTextField tfSigla;
+
 	/**
 	 * Create the panel.
 	 */
 	public TelaAlterarDepartamento(CadastroDepartamento cadDep) {
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-
-		JButton btnAlterar = new JButton("Alterar");
-		btnAlterar.setBounds(106, 215, 100, 25);
-		panel.add(btnAlterar);
 
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addMouseListener(new MouseAdapter() {
@@ -50,35 +48,69 @@ public class TelaAlterarDepartamento extends JPanel {
 		JLabel lblCodigoDoDepartamento = new JLabel("Insira o código do departamento");
 		lblCodigoDoDepartamento.setBounds(146, 50, 195, 16);
 		panel.add(lblCodigoDoDepartamento);
-		
+
 		JLabel lblAlterar = new JLabel("Alterar dados");
 		lblAlterar.setBounds(184, 24, 129, 14);
 		panel.add(lblAlterar);
-		
+
 		JLabel lblDados = new JLabel("Dados");
 		lblDados.setBounds(99, 116, 51, 14);
 		panel.add(lblDados);
-		
-		JButton btnConsultar = new JButton("Consultar");
-		btnConsultar.setBounds(246, 75, 89, 23);
-		panel.add(btnConsultar);
-		
+
 		tfNome = new JTextField();
 		tfNome.setBounds(120, 142, 86, 20);
 		panel.add(tfNome);
 		tfNome.setColumns(10);
-		
+		tfNome.setEditable(false);
+
 		tfSigla = new JTextField();
 		tfSigla.setBounds(264, 140, 86, 20);
 		panel.add(tfSigla);
 		tfSigla.setColumns(10);
-		
+		tfSigla.setEditable(false);
+
 		JLabel lblNome = new JLabel("Nome");
 		lblNome.setBounds(75, 145, 43, 14);
 		panel.add(lblNome);
-		
+
 		JLabel lblSigla = new JLabel("Sigla");
 		lblSigla.setBounds(227, 143, 36, 14);
 		panel.add(lblSigla);
+
+		JButton btnConsultar = new JButton("Consultar");
+		btnConsultar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Departamento dpmt = DepartamentoDAO.pesquisa(Integer.parseInt(txtCodigo.getText()));
+				lblNome.setText("Nome" + dpmt.getNome());
+				lblSigla.setText("Sigla" + dpmt.getSigla());
+			}
+		});
+		btnConsultar.setBounds(246, 75, 89, 23);
+		panel.add(btnConsultar);
+
+		JButton btnAlterar = new JButton("Alterar");
+		btnAlterar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (!lblNome.getText().equals("Nome") && !lblSigla.getText().equals("Sigla")) {
+					Departamento dpmt = new Departamento();
+					
+					tfNome.setEditable(true);
+					tfSigla.setEditable(true);
+					
+					dpmt.setCodigo(Integer.parseInt(txtCodigo.getText()));
+					dpmt.setNome(tfNome.getText());
+					dpmt.setSigla(tfSigla.getText());
+					
+					DepartamentoDAO.alterar(dpmt);
+				} else {
+					JOptionPane.showMessageDialog(panel, "Consulte o codigo antes!", "Waring",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
+		btnAlterar.setBounds(106, 215, 100, 25);
+		panel.add(btnAlterar);
 	}
 }
--- src/TelaExcluirDepartamento.java
+++ src/TelaExcluirDepartamento.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -14,12 +15,13 @@ public class TelaExcluirDepartamento extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField txtCodigo;
+
 	/**
 	 * Create the panel.
 	 */
 	public TelaExcluirDepartamento(CadastroDepartamento cadDep) {
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
@@ -27,15 +29,42 @@ public class TelaExcluirDepartamento extends JPanel {
 		JLabel lblCodigoDoDepartamento = new JLabel("Insira o código do departamento");
 		lblCodigoDoDepartamento.setBounds(131, 36, 198, 16);
 		panel.add(lblCodigoDoDepartamento);
-		
+
+		JLabel lblDados = new JLabel("Dados");
+		lblDados.setBounds(96, 101, 46, 14);
+		panel.add(lblDados);
+
+		JLabel lblNome = new JLabel("Nome: ");
+		lblNome.setBounds(116, 146, 198, 14);
+		panel.add(lblNome);
+
+		JLabel lblSigla = new JLabel("Sigla: ");
+		lblSigla.setBounds(116, 164, 100, 14);
+		panel.add(lblSigla);
+
+		JLabel lblCodigo = new JLabel("Codigo: ");
+		lblCodigo.setBounds(116, 126, 100, 14);
+		panel.add(lblCodigo);
+
 		txtCodigo = new JTextField();
 		txtCodigo.setBounds(96, 70, 120, 20);
 		panel.add(txtCodigo);
 		txtCodigo.setColumns(10);
-		
-		JButton btnConsultar = new JButton("Excluir");
-		btnConsultar.setBounds(96, 216, 100, 25);
-		panel.add(btnConsultar);
+
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (!lblNome.getText().equals("Nome: ") && !lblSigla.getText().equals("Sigla: ")
+						&& !lblCodigo.getText().equals("Codigo: ")) {
+					DepartamentoDAO.deletar(Integer.parseInt(txtCodigo.getText()));
+				} else {
+					JOptionPane.showMessageDialog(panel, "Consulte o codigo antes!", "Waring", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+		});
+		btnExcluir.setBounds(96, 216, 100, 25);
+		panel.add(btnExcluir);
 
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addMouseListener(new MouseAdapter() {
@@ -48,30 +77,24 @@ public class TelaExcluirDepartamento extends JPanel {
 		});
 		btnVoltar.setBounds(214, 216, 100, 25);
 		panel.add(btnVoltar);
-		
+
 		JLabel lblExcluir = new JLabel("Excluir");
 		lblExcluir.setBounds(195, 11, 46, 14);
 		panel.add(lblExcluir);
-		
-		JButton btnConsultar_1 = new JButton("Consultar");
-		btnConsultar_1.setBounds(226, 69, 89, 23);
-		panel.add(btnConsultar_1);
-		
-		JLabel lblDados = new JLabel("Dados");
-		lblDados.setBounds(96, 101, 46, 14);
-		panel.add(lblDados);
-		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(116, 146, 198, 14);
-		panel.add(lblNome);
-		
-		JLabel lblSigla = new JLabel("Sigla");
-		lblSigla.setBounds(116, 164, 100, 14);
-		panel.add(lblSigla);
-		
-		JLabel lblCodigo = new JLabel("Codigo");
-		lblCodigo.setBounds(116, 126, 100, 14);
-		panel.add(lblCodigo);
+
+		JButton btnConsultar = new JButton("Consultar");
+		btnConsultar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Departamento dpmt = DepartamentoDAO.pesquisa(Integer.parseInt(txtCodigo.getText()));
+				lblCodigo.setText("Codigo: " + dpmt.getCodigo());
+				lblNome.setText("Nome: " + dpmt.getNome());
+				lblSigla.setText("Sigla: " + dpmt.getSigla());
+			}
+		});
+		btnConsultar.setBounds(226, 69, 89, 23);
+		panel.add(btnConsultar);
+
 	}
 
 }
